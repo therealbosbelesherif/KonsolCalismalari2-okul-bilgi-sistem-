f91baa64d3bdee9bbe36656dace27ea677c9869e
Berke Aydoğan <lutfentekrar2@hotmail.com>
2022-04-12 14:21:39 +0300

    Update Program.cs

--- KonsolCalismalari2/Program.cs
+++ KonsolCalismalari2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,24 +11,27 @@ namespace KonsolCalismalari2
         static void Main(string[] args)
         {
             Console.WriteLine("***OGRENCI BILGI SISTEM***");
-            int ogrencisayi,i, not;
+            int ogrencisayi,i;
             Console.WriteLine("Ogrenci sayisini giriniz: ");
             ogrencisayi = Convert.ToInt32(Console.ReadLine());
             string[] isim = new string[ogrencisayi];
             for(i = 0; i < ogrencisayi; i++)
             {
+                Console.Clear();
                 Console.WriteLine("{0}. öğrencinin ismini giriniz: ", i + 1);
                 isim[i] = Console.ReadLine();
             }
             string[] soyisim = new string[ogrencisayi];
             for (i = 0; i < ogrencisayi; i++)
             {
+                Console.Clear();
                 Console.WriteLine("{0}. öğrencinin soyismini giriniz: ", i + 1);
                 soyisim[i] = Console.ReadLine();
             }
             string[] numara = new string[ogrencisayi];
             for (i = 0; i < ogrencisayi; i++)
             {
+                Console.Clear();
                 Console.WriteLine("{0}. öğrencinin numarasını giriniz: ", i + 1);
                 numara[i] = Console.ReadLine();
             }
@@ -38,6 +41,7 @@ namespace KonsolCalismalari2
             int[] sinav3 = new int[ogrencisayi];
             for (i = 0; i < ogrencisayi; i++)
             {
+                Console.Clear();
                 Console.WriteLine("{0} {1} ({2}) isimli öğrencinin birinci sınav notunu giriniz: ", isim[i], soyisim[i], numara[i]);
                 sinav1[i] = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("{0} {1} ({2}) isimli öğrencinin ikinci sınav notunu giriniz: ", isim[i], soyisim[i], numara[i]);
@@ -46,6 +50,7 @@ namespace KonsolCalismalari2
                 sinav3[i] = Convert.ToInt32(Console.ReadLine());
                 sinavortalama[i] = (sinav1[i] + sinav2[i] + sinav3[i]) / 3;
             }
+            Console.Clear();
             Console.WriteLine("**********OGRENCI NOT SISTEM**********");
             for (i = 0; i < ogrencisayi; i++)
             {
